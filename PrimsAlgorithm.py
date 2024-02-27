58f20c3ad49b84e5207a5431329aa62e1827d36b
Smit <smitmpatil123@gmail.com>
2024-02-27 15:41:32 +0530

    IMPACT TRAINING

---
+++ PrimsAlgorithm.py
@@ -0,0 +1,7 @@
+# Prims Algorithm
+
+# Graph should be weighted connected and undirected
+#     Step1:- Consider any vertices 
+#     Step2:- Find all edges selected vertex to all new vertices , select least weighted edges and included in a tree , if least weighted edges form a circle than discarded and consider next least weighted edges and include in a tree minimum spanning tree
+#     Step3:- perform step2 until all vertices included in a tree
+
